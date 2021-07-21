61efe61d81f4365ab1ef7755fca22b017a21c376
AkshayHambir <hambirakshay379@gmail.com>
2021-07-21 19:05:49 +0530

    footer updated

--- internal/footer.php
+++ internal/footer.php
@@ -2,8 +2,9 @@
     <div class="container-fluid text-center">
         <div class="m-auto">
             <span class="text-white">
-                &#169; copyright 2021 : Akshay Hambir
+                &#169; All Copyrights Reserved 2021 :@ Akshay Hambir
             </span>
+            <p><a href="https://www.linkedin.com/in/akshay-hambir-9629ba190/" target="_blank"><i class="fab fa-linkedin"></i></a> <a href="https://github.com/AkshayHambir" target="_blank"><i class="fab fa-github-square"></i></a></p>
         </div>
 
     </div>
